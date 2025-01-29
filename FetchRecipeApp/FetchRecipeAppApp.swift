5e833db8d0006bb4fdd35996d8c0d19a44fd6426
gantab665 <ganta.b@northeastern.edu>
2025-01-29 13:24:00 -0500

    Initial Commit

---
+++ FetchRecipeApp/FetchRecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeAppApp.swift
+//  FetchRecipeApp
+//
+//  Created by Jerry on 1/29/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FetchRecipeAppTests/FetchRecipeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeAppTests.swift
+//  FetchRecipeAppTests
+//
+//  Created by Jerry on 1/29/25.
+//
+
+import Testing
+@testable import FetchRecipeApp
+
+struct FetchRecipeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
